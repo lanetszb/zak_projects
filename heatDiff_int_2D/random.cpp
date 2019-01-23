ae75ada37dfe230765bde47b7b6d313deff02483
Zakhar Lanets <lanetszb@gmail.com>
2019-01-23 14:31:54 +0500

    heatDiff_int_2D: created random.cpp and .h, moved Jacobi there

---
+++ heatDiff_int_2D/random.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mr Zakhar Lanetc on 2019-01-23.
+//
+
+#include "random.h"
---
+++ heatDiff_int_2D/random.h
@@ -0,0 +1,8 @@
+//
+// Created by Mr Zakhar Lanetc on 2019-01-23.
+//
+
+#ifndef HEATDIFF_INT_2D_RANDOM_H
+#define HEATDIFF_INT_2D_RANDOM_H
+
+#endif //HEATDIFF_INT_2D_RANDOM_H
