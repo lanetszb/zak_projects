badcbd3dbe730be29fafd80cbf5ffcc72db082ca
Zakhar Lanets <lanetszb@gmail.com>
2019-09-26 14:39:34 +1000

    zak_projects, heatDiff_int_2D: added colorconsole

---
+++ colorconsole/ColorConsole.h
@@ -0,0 +1,23 @@
+#ifndef COLORCONSOLE_H
+#define COLORCONSOLE_H
+
+
+const std::string red("\033[0;31m");
+const std::string lred("\033[1;31m");
+const std::string green("\033[0;32m");
+const std::string lgreen("\033[1;32m");
+const std::string blue("\033[0;34m");
+const std::string lblue("\033[1;34m");
+const std::string purple("\033[0;35m");
+const std::string lpurple("\033[1;35m");
+const std::string yellow("\033[0;33m");
+const std::string lyellow("\033[1;33m");
+const std::string cyan("\033[0;36m");
+const std::string lcyan("\033[1;36m");
+const std::string lgrey("\033[0;37m");
+const std::string dgrey("\033[1;30m");
+
+const std::string resetColor("\033[0m");
+
+
+#endif
---
+++ colorconsole/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ColorConsole.h>
+
+int main() {
+
+    std::cout << red << "Hello, World!" << std::endl;
+    std::cout << lred << "Hello, World!" << std::endl;
+    std::cout << green << "Hello, World!" << std::endl;
+    std::cout << lgreen << "Hello, World!" << std::endl;
+    std::cout << blue << "Hello, World!" << std::endl;
+    std::cout << lblue << "Hello, World!" << std::endl;
+    std::cout << purple << "Hello, World!" << std::endl;
+    std::cout << lpurple << "Hello, World!" << std::endl;
+    std::cout << yellow << "Hello, World!" << std::endl;
+    std::cout << lyellow << "Hello, World!" << std::endl;
+    std::cout << cyan << "Hello, World!" << std::endl;
+    std::cout << lcyan << "Hello, World!" << std::endl;
+    std::cout << lgrey << "Hello, World!" << std::endl;
+    std::cout << dgrey << "Hello, World!" << std::endl;
+    std::cout << std::endl;
+    std::cout << resetColor << "Hello, World!" << std::endl;
+
+    return 0;
+}
